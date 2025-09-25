3686d728765bdeef2c444f155382d6b859b4debc
Danie <danieleah43@gmail.com>
2025-09-25 19:47:32 +0500

    messageeeeee

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ function App() {
       <h1>Automation Done </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is not {count}
+          count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR, We have completed code coverage too i guess
+          Edit <code>src/App.jsx</code> and save to test HMR, We have completed code coverage too
         </p>
       </div>
       <p className="read-the-docs">
